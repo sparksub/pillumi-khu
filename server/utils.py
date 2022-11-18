1a876109bb8ca2ad209c0897e98b575a1d02171f
Subin Park <lmhapy25@khu.ac.kr>
2022-11-18 15:29:48 +0900

    Change README.md & .gitignore

--- server/utils.py
+++ server/utils.py
@@ -1,3 +1,3 @@
-serviceKey = "<YOUR DECODE KEY>"
+serviceKey = "1+jQerEB5bUgoo7GNXrQR4b2bKwDMe+9M9pWE5zdBpoiHxX9ByldXHNaS1HLgS/HeT/B9Vd9kIFY3lgVlwstIQ=="
 
-ip = "<YOUR IP ADDRESS>"
+ip = '172.30.1.46'
